2085e1a1abaca715dc3758b88ee28ed9049df559
Harshil Gupta <harshilgupta.email@gmail.com>
2025-07-21 18:07:40 +0530

    Minor Changes to server.js and nodemon installation to project for restarting project due to  change in app + information on Route and Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import express from "express";
 
 const app = express()
 
-//built first dummy api ⤵️
+// What is a Route
+// In Node.js, particularly when building web applications with frameworks like Express.js, routes define how an application responds to client requests for specific endpoints.
+// What is an Endpoint?
+// An endpoint is a combination of a URL + HTTP Method that lets the client interact with a specific resource or server
 app.get("/api/notes", (req, res) => {
     // send the notes
-    res.status(200).send("you got 5 notes");
+    res.status(200).send("you got 40 notes");
 })
-
+// 👆 this is a route
 app.listen(5001, () => {
   console.log("Server is running on port 5001")
 });
