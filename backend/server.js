e652bf4615e72dfca0b56830eaa2f65f286d11ad
Harshil Gupta <harshilgupta.email@gmail.com>
2025-07-21 15:31:35 +0530

    Dummy api making with basic express@4.18.2 installation and setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+// const express = require("express");
+
+const app = express()
+
+//built first dummy api ⤵️
+app.get("/api/notes", (req, res) => {
+    res.send("you got 5 notes");
+})
+
+app.listen(5001, () => {
+  console.log("Server is running on port 5001")
+});
