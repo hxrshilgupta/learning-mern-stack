15295265b7a34e83797d1d425da1ccadd0d2d282
Harshil Gupta <harshilgupta.email@gmail.com>
2025-08-02 04:37:21 +0530

    minor changes to notesController.js

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,10 +1,17 @@
-export function getAllNotes(req, res) {
-  res.status(200).send("You just fetched the notes");
+import Note from "../models/Note.js";
+
+export async function getAllNotes(req, res) {
+  try{
+    //fetch all notes from the database
+    const notes = await Note.find(); //to get all notes
+    //Standard Mongoose query function
+    res.status(200).json(notes);
+  }
+  catch (error) {
+    console.error("Error in getAllNotes controller:", error);
+    res.status(500).json({message: "Internal Server Error"});
+  }
 };
-// export const getAllNotes = (req, res) => {
-//   res.status(200).send("You just fetched the notes");
-// };
-// ----> OTHER WAY OF WRITING THE ABOVE FUNCTION
 
 export function createNotes(req, res) {
   res.status(201).json({ message: "Note Created Successfully" });
