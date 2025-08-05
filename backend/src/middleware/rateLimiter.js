9009933acaebe2db128be446a962cf425293042d
Harshil Gupta <harshilgupta.email@gmail.com>
2025-08-05 19:02:13 +0530

    Minor fixes

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,15 +1,15 @@
 import rateLimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
+    // Implement rate limiting logic here
     try{
         const {success} = await rateLimit.limit("my-limit-key");
-
+        // we can implement limits in different identifiers (userId, IP address, etc.)
         if(!success){
             return res.status(429).json({
                 message: "Too Many Requests, please try again later"
             });
         }
-        
         // Call next() to proceed to the next middleware/route handler
         next();
     }catch(error){
--- backend/src/server.js
+++ backend/src/server.js
@@ -9,17 +9,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middleware to parse JSON requests
-app.use(express.json());
-
+app.use(express.json()); // Middleware to parse JSON requests
+app.use(rateLimiter); //custom middleware that we just created
 //Middleware is a function that runs in the middle between the request and response
 
-app.use(rateLimiter);
-//custom middleware that we just created
+app.use("/api/notes", notesRoutes); // Routes
 
-// Routes
-app.use("/api/notes", notesRoutes);
-
+/**
+ * Best Practice: Connect to the database before starting the server
+ * This ensures that the server only starts if the database connection is successful
+ * If the database connection fails, we can log the error and exit the process
+ * This prevents the server from running in an inconsistent state where it cannot access the database.
+ * This is especially important for applications that rely on database operations for their functionality.
+ * It helps in maintaining the integrity and reliability of the application.
+ * This is a common pattern in Node.js applications to ensure that the server is ready to handle
+*/
 // Start server function
 const startServer = async () => {
   try {
