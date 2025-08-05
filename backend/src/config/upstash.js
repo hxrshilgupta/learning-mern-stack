18e9bb10b82fae079241c4a4af7bf6dc333d86d7
Harshil Gupta <harshilgupta.email@gmail.com>
2025-08-05 18:22:18 +0530

    RateLimiter issue fixed

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,13 +1,14 @@
-import pkg from "@upstash/ratelimit";
-const { Ratelimit } = pkg;
+import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
+
 import dotenv from "dotenv";
 
 dotenv.config();
-// create a ratelimiter that allows 5 requests per 10 seconds
-const rateLimit = new Ratelimit({
-    redis: Redis.fromEnv(), // accessing environment variables
-    limiter: Ratelimit.slidingWindow(10, "10 s")
+
+// create a ratelimiter that allows 100 requests per minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5, "60 s"),
 });
 
-export default rateLimit;
+export default ratelimit;
--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -9,6 +9,9 @@ const rateLimiter = async (req, res, next) => {
                 message: "Too Many Requests, please try again later"
             });
         }
+        
+        // Call next() to proceed to the next middleware/route handler
+        next();
     }catch(error){
         console.log("Rate Limit Error", error);
         next(error);
